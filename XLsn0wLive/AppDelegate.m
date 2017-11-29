44a1f700efc725be87bab144b57a1f421ad24307
xlsn0w <xlsn0w@outlook.com>
2017-11-29 17:41:49 +0800

    update

--- XLsn0wLive/AppDelegate.m
+++ XLsn0wLive/AppDelegate.m
@@ -13,3 +13,27 @@
 }
 ///Objc 中发送消息是用中括号（[]）把接收者和消息括起来，而直到运行时才会把消息与方法实现绑定。
 @end
+
+
+/*
+ 
+ 
+ - 初始化UIApplication单例对象
+ - 初始化AppDelegate对象，并设为UIApplication对象的代理
+ - 检查Info.plist设置的xib文件是否有效，如果有则解冻Nib文件并设置outlets，创建显示key window、rootViewController、与rootViewController关联的根view（没有关联则看rootViewController同名的xib），否则launch之后由程序员手动加载。
+ - 建立一个主事件循环，其中包含UIApplication的Runloop来开始处理事件。
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+*/
